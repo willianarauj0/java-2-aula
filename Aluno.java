6448aeb30802c32648f729ad60aafed4fe322583
willianarauj0 araujo <willianeves66@gmail.com>
2023-08-15 19:37:22 +0000

    att

---
+++ Aluno.java
@@ -0,0 +1,21 @@
+public class Aluno {
+    private String nome;
+    private int idade;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return idade;
+
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
---
+++ Exemplo.java
@@ -0,0 +1,9 @@
+public class Exemplo {
+    public static void main(String[]args) {
+        Aluno al = new Aluno();
+        al.setNome("will DF");
+
+        System.out.println(al.getNome());
+    }
+    
+}
